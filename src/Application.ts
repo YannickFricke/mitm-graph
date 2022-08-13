7fd1499f9fc379d05f2052e26d5f5c600bc13483
Yannick Fricke <yannickfricke@googlemail.com>
2022-08-13 20:47:27 +0200

    implemented the first logic for refreshing the token

--- src/Application.ts
+++ src/Application.ts
@@ -1,12 +1,26 @@
 import { inject, injectable } from 'inversify';
 import { Logger } from 'tslog';
+import { TwitchToken } from './twitch/token';
 
 @injectable()
 export class Application {
 	constructor(
 		@inject(Logger)
 		private readonly logger: Logger,
+
+		@inject(TwitchToken)
+		private readonly twitchToken: TwitchToken,
 	) {}
 
-	public async run() {}
+	public async run() {
+		this.logger.info('Starting application.');
+
+		this.twitchToken.load_token();
+
+		if (this.twitchToken.needs_refresh()) {
+			await this.twitchToken.refresh_token();
+		}
+
+		this.twitchToken.save_token();
+	}
 }
